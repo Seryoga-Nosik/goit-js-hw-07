2dec6ef0b300e6b1141475fb592f9a976875bf16
Sergey <sergey.nosan@gmail.com>
2021-04-30 18:43:06 +0300

    add first task

--- js/task-01.js
+++ js/task-01.js
@@ -4,5 +4,13 @@
 Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
 Например для первой категории получится:
 Категория: Животные
-Количество элементов: 4
- */
+Количество элементов: 4 */
+const itemEl = document.querySelectorAll('.item');
+console.log(`В списке ${itemEl.length} категории.`);
+
+itemEl.forEach(element => {
+  console.log(
+    `Категория: ${element.firstElementChild.textContent}
+    количество элементов: ${element.lastElementChild.children.length}`,
+  );
+});
